49f8e665fc61b79482415484f2c814cb93b78010
izanaqi0606-hash <izanaqi0606@gmail.com>
2025-10-28 23:38:23 +0700

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,21 @@
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
+
+typedef int infotype;
+typedef int address;
+
+
+typedef struct {
+    infotype info[MAXSTACK];
+    address top;
+} stack;
+
+void createStack_103032400028(Stack *s);
+bool isEmpty_103032400028(Stack s);
+bool isFull_103032400028(Stack s);
+void push_103032400028(Stack *s, infotype X);
+infotype pop_103032400028(Stack *s);
+infotype sumStack_103032400028(stack s);
+
+
+#endif
